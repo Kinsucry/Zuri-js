2dc0c47074bc59ee3621bd0427720519ddc4bac8
Williams Stephanie <105182839+Kinsucry@users.noreply.github.com>
2022-06-20 19:52:21 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+var myName = "Stephanie Williams"
+var height = 5-6
+var country = "Nigerian"
+
+document.write('My name is' + ' ' + myName + '<br>')
+document.write("I'm "+ ' ' + height + ' ' +'feet tall' +'<br>')
+document.write("Am a" + ' ' + country)
